c5196018a7787ea4c592fda1ff890a7bab05fec5
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2023-06-21 15:57:32 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+  if(isset($_POST['submit']) && isset($_POSt['message'])) {
+    $message= $POST['message'];
+    $message = htmlspecialchars($message, ENT_QUOTES, 'UTF-8');
+    $url = filter_var($message,FILTER_VALIDATE_URL);
+  }
+?>
+
+<DOCTYPE html>
+<html>
+  <head>
+    <title>Cas pratique</title>
+  </head>
+  <body>
+ <h1>Formulaire</h1>
+    <form method = "post">
+      <label for="message">Message :</label><br>
+      <textarea type="text" name="message" id="message" rows="5" cols="40">
+        <?php if($url !==false ) {echo "lien interdit";} else{ echo $message ; } ?>
+      </textarea><br>
+      <input type="submit" value=" Soumettre" name="submit">
+    </form>
+<!-- Afin de sécuriser davantage une application , il est possible d'empécher les utilisateurs de soumettre des liens dans les messages Pour celz , on peut utiliser mla fonction filter_var() avec lm'option FILTER_VALIDATE_URL pour valider si le contenu de la variable $message est un lien valide.Si la validation réussit , cela indique que l'utilisateur a sousmis un lien , et un message d'erreurpeut etre affiché à la place du lien pour avertir l'utilisateur qu'il n'est pas autorisé à soumettre des liens -->    
+    
+  </body>
+</html>
